81ab4e46d13b67d474218195057593c98f9db440
Webdown <que.babault@gmail.com>
2020-05-21 16:25:40 +0200

    add api backbone

--- .github/actions/deploy-customization/entrypoint.sh
+++ .github/actions/deploy-customization/entrypoint.sh
@@ -7,7 +7,8 @@ commit_hash=$3
 echo "Kubernetes config files customization..."
 
 cd ${deploy_directory}/overlays/${stage}
-$(kustomize edit set image eu.gcr.io/open-blueprint/frontend:${commit_hash})
+$(kustomize edit set image eu.gcr.io/aperture-devs/obp-frontend-webserver:${commit_hash})
+$(kustomize edit set image eu.gcr.io/aperture-devs/obp-api-php:${commit_hash})
 kustomize build -o ./../../manifest.yaml
 echo ::set-output name=manifest-path::${deploy_directory}/manifest.yaml
 echo "Kubernetes config files customization done"
